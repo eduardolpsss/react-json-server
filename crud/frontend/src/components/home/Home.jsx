7bbca451dfa90f66dfea90605d7321340188ed2d
Eduardo Pontes <eduardo.pontes2801@gmail.com>
2022-11-01 23:06:48 -0300

    frontEndRoutesImplementation

--- crud/frontend/src/components/home/Home.jsx
+++ crud/frontend/src/components/home/Home.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Main from '../template/Main'
 
-export default props =>
+export default props => 
     <Main icon='home' title='Home' subtitle='CRUD developed with React.js on the front-end and a JSON Server back-end.'>
     <div className="display-4">
-        Bem-vindo!
+        Welcome!
     </div>
     <hr />
     <p className='mb-0'>
